1ae5d3fbd0470ac23e519b31047b6829dbf6b292
mirmik <mirmikns@yandex.ru>
2021-06-03 17:18:33 +0300

    'Add licant build script'

---
+++ make.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import licant 
+
+licant.cxx_application("rfdaemon",
+	sources = [
+		"main.cpp",
+		"App.cpp",
+		"AppManager.cpp",
+		"TcpServer.cpp",
+		"RFDaemonServer.cpp"
+	],
+	libs = ["jsoncpp", "igris", "pthread"]
+)
+
+licant.ex("rfdaemon")
