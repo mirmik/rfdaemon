890cde5c1bb614baaff1be1208abff2d83895a23
mirmik <mirmikns@yandex.ru>
2022-07-22 18:48:02 +0300

    'a'

--- src/Beam.cpp
+++ src/Beam.cpp
@@ -1,4 +1,11 @@
 #include <Beam.h>
+#include <igris/trent/json.h>
+#include <igris/trent/json_print.h>
+
+std::map<std::string, igris::trent(Beam::*)(const igris::trent&)> Beam::method_map = {
+    { "getsettings", &Beam::getsettings },
+};
+
 
 Beam::Beam()
 {
@@ -21,6 +28,34 @@ void Beam::beam_thread_func()
     for (;;)
     {
         auto [data, ip, port] = recv_socket.recvfrom();
-        nos::println(data);
+        nos::println("NewBeamMessage:", data);
+
+        try 
+        {
+            igris::trent tr = igris::json::parse(data);
+            on_new_command(tr, ip, port);
+        } 
+        catch (const std::exception& e)
+        {
+            nos::println("Beam error:", e.what());
+        }
     }
+}
+
+igris::trent Beam::getsettings(const igris::trent& tr) 
+{
+    return tr;
+}
+
+void Beam::on_new_command(const igris::trent& tr, std::string ip, int port) 
+{
+    if (!tr.have("cmd")) 
+    {
+        throw std::runtime_error("cmd field is required");
+    }
+    auto cmd = tr["cmd"].as_string();
+
+    auto rettr = (this->*method_map[cmd])(tr);
+    auto json = igris::json::to_string(rettr, false);
+    send_socket.sendto(json.data(), json.size(), ip, port);
 }
--- src/Beam.h
+++ src/Beam.h
@@ -3,9 +3,12 @@
 
 #include <nos/inet/udp_socket.h>
 #include <thread>
+#include <map>
+#include <igris/trent/trent.h>
 
 class Beam
 {
+    static std::map<std::string, igris::trent(Beam::*)(const igris::trent&)> method_map;
     nos::inet::udp_broadcast_socket recv_socket;
     nos::inet::udp_broadcast_socket send_socket;
     std::thread beam_thread;
@@ -16,6 +19,9 @@ class Beam
 public:
     Beam();
     void start();
+    void on_new_command(const igris::trent& tr, std::string ip, int port);
+
+    igris::trent getsettings(const igris::trent& tr); 
 
 private:
     void beam_thread_func();
