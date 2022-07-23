96798a87aace9e8cb6babfb2298f5c9ca4792623
mirmik <mirmikns@yandex.ru>
2022-07-23 19:59:44 +0300

    'a'

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -4,8 +4,12 @@
 #include <memory>
 #include <nos/io/sstream.h>
 #include <nos/fprint.h>
+#include <thread>
+#include <chrono>
 
 extern std::unique_ptr<AppManager> appManager;
+using namespace nos::argument_literal;
+using namespace std::chrono_literals;
 
 void start_httpserver() 
 {
@@ -16,16 +20,52 @@ void start_httpserver()
 		// HTTPS
 //		httplib::SSLServer svr;
 
-		svr.Get("/", [](const httplib::Request &, httplib::Response &res) {
+		svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
+			std::string cmd = req.get_param_value("cmd");
+			
+			if (cmd == "stop") 
+			{
+				int arg = std::stoi(req.get_param_value("arg"));
+				nos::println(cmd, arg);
+				appManager->applications()[arg].stop();
+				std::this_thread::sleep_for(200ms);
+			}
+
+			if (cmd == "start") 
+			{
+				int arg = std::stoi(req.get_param_value("arg"));
+				nos::println(cmd, arg);
+				appManager->applications()[arg].start();
+				std::this_thread::sleep_for(200ms);
+			}
 
 			auto& apps = appManager->applications();
 			nos::stringstream ss;
-    		for (auto &app : apps)
-        		nos::fprintln_to(ss, "{} : {} : {}", app.name(), app.status_string(),
-                    app.pid());
+    		for (size_t i=0; i<apps.size(); i++)
+			{
+				auto& app = apps[i];
+				std::string button_stop = nos::format(R"(<button type="button" onclick="window.location.href='/?cmd=start&arg={}';">start</button>)", i);
+				std::string button_start = nos::format(R"(<button type="button" onclick="window.location.href='/?cmd=stop&arg={}';">stop</button>)", i);
+        		nos::fprintln_to(ss, R"(<p>{} : {} : {} {} {})", app.name(), app.status_string(),
+                    app.pid(), button_stop, button_start);
+			}
+			std::string text = nos::format(R"(
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Тег BUTTON</title>
+ </head>
+ <body>
+  <p style="text-align: center"><button>Кнопка с текстом</button>
+  <button><img src="images/umbrella.gif" alt="Зонтик" 
+         style="vertical-align: middle"> Кнопка с рисунком</button></p>
 
-			std::string text = "Hello World!\n\n" + ss.str(); 
-			res.set_content(text, "text/plain");
+		 {tasks}
+ </body>
+</html>
+			)", "tasks"_a=ss.str()); 
+			res.set_content(text, "text/html");
 		});
 		
 		svr.listen("0.0.0.0", 8080);
